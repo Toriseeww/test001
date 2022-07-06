eaf71a04532040dc4424d52f86d3f77649b37662
WCM <1370178953@qq.com>
2022-07-06 14:58:37 +0800

    first commit

---
+++ src/main/java/com/example/test001/Test001Application.java
@@ -0,0 +1,13 @@
+package com.example.test001;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.example.test001.mapper")
+public class Test001Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Test001Application.class, args);
+    }
+}
